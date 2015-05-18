d0fa4b10d7d7103bcfdfc02edbedb9ff539bbc3c
Jurlind_Budurushi <jurlindbudurushi@gmail.com>
2015-05-18 16:13:20 +0200

    Adding the copyright text.

--- vcd_votedevice/src/main/java/de/tud/vcd/votedevice/tuTestElection/TUTestElectionContainer.java
+++ vcd_votedevice/src/main/java/de/tud/vcd/votedevice/tuTestElection/TUTestElectionContainer.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * #  Copyright 2015 SecUSo.org / Jurlind Budurushi / Roman Jöris
+ * #
+ * #  Licensed under the Apache License, Version 2.0 (the "License");
+ * #  you may not use this file except in compliance with the License.
+ * #  You may obtain a copy of the License at
+ * #
+ * #      http://www.apache.org/licenses/LICENSE-2.0
+ * #
+ * #  Unless required by applicable law or agreed to in writing, software
+ * #  distributed under the License is distributed on an "AS IS" BASIS,
+ * #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * #  See the License for the specific language governing permissions and
+ * #  limitations under the License.
+ *******************************************************************************/
 package de.tud.vcd.votedevice.tuTestElection;
 
 import de.tud.vcd.common.BallotCardDesign;
